08708c9fe4335af298e4bf562b0f45c94fbda756
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-03-06 10:25:12 -0500

    Mutual guilds in friends menu added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The tab bar is now working!
 ![image](https://user-images.githubusercontent.com/54505527/110188905-662ddb80-7deb-11eb-8f8b-3246d8bbbe3d.png)
 
 # Todos:
-[] - Add mutual guilds in friend menu
+[x] - Add mutual guilds in friend menu
 
 [] - Add the search feature that the bd version has
 
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { Tooltip } = require('powercord/components');
 
 module.exports = class betterfriendslist extends Plugin {
 	startPlugin() {
+		this.loadStylesheet('style.css');
 		const TabBar = getModuleByDisplayName('TabBar', false).prototype;
+		const PeopleListItem = getModule(m => m.displayName === 'FriendRow', false).prototype;
 		inject('bfl-tabbar', TabBar, 'render', (_, res) => {
 			let relationships = getModule(['getRelationships'], false).__proto__.getRelationships(),
 				relationshipCount = {};
@@ -34,10 +36,47 @@ module.exports = class betterfriendslist extends Plugin {
 				}
 			return res;
 		});
+
+		inject('bfl-peoplelist', PeopleListItem, 'render', (args, res) => {
+			if (typeof res.props.children === 'function') {
+				if (res._owner.pendingProps.mutualGuilds.length !== 0) {
+					let childrenRender = res.props.children;
+					res.props.children = (...args) => {
+						let children = childrenRender(...args);
+						children.props.children.splice(1, 0, React.createElement('div', { className: 'mutualGuilds-s7F2aa container-5VyO4t' }));
+						res._owner.pendingProps.mutualGuilds.forEach((guild, idx) => {
+							const icon = React.createElement(Tooltip, {
+								text: guild.name,
+								position: 'top',
+								children: React.createElement(
+									'div',
+									{
+										className: res._owner.pendingProps.mutualGuilds[idx + 1]
+											? 'iconContainerMasked-G-akdf iconContainer-IBAtWs'
+											: 'iconContainer-IBAtWs',
+									},
+									React.createElement('div', {
+										className: 'icon-3o6xvg icon-r6DlKo iconSizeSmaller-2whVAO iconInactive-98JN5i',
+										style: {
+											backgroundImage: `url("https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=256")`,
+										},
+									})
+								),
+							});
+							if (!children.props.children[1].props.children) children.props.children[1].props.children = [icon];
+							else children.props.children[1].props.children.push(icon);
+						});
+						return children;
+					};
+				}
+			}
+			return res;
+		});
 	}
 
 	pluginWillUnload() {
 		uninject('bfl-tabbar');
+		uninject('bfl-peoplelist');
 	}
 
 	createBadge(amount, text) {
